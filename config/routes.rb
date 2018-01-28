86b963cf7dd458d47cbd162c137193464713bcee
Greg <GregJieNing@gmail.com>
2018-01-29 01:09:45 +0800

    admin can see resume

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     member do
     post :publish
     post :hide    
-    end    
+    end  
+    resources :resumes  
   end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
